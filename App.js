d62ba8289305d3cfd3d5fd79157c7b3d434b2ad3
bilalhasananideos <bilal.hasananideos@gmail.com>
2024-01-02 18:10:43 +0500

    Carousal Done Flatlist Expo

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import HorizontalCarousal from "./src/components/HorizontalCarousal";
 
+const data = [
+  {
+    id: 0,
+    title: "Lady with a Teddy",
+    image: "https://images.pexels.com/photos/3348748/pexels-photo-3348748.jpeg",
+  },
+  {
+    id: 1,
+    title: "Girl with camera",
+    image: "https://images.pexels.com/photos/3812944/pexels-photo-3812944.jpeg",
+  },
+  {
+    id: 2,
+    title: "Beautiful Girl with Glasses",
+    image: "https://images.pexels.com/photos/2100063/pexels-photo-2100063.jpeg",
+  },
+  {
+    id: 3,
+    title: "Redhead with frackles",
+    image: "https://images.pexels.com/photos/3228213/pexels-photo-3228213.jpeg",
+  },
+  {
+    id: 4,
+    title: "Girl in black dress",
+    image: "https://images.pexels.com/photos/1385472/pexels-photo-1385472.jpeg",
+  },
+  {
+    id: 5,
+    title: "Girl Sitting on Chair",
+    image: "https://images.pexels.com/photos/4725133/pexels-photo-4725133.jpeg",
+  },
+  {
+    id: 6,
+    title: "Girl Sitting on Chair",
+    image: "https://images.pexels.com/photos/4725133/pexels-photo-4725133.jpeg",
+  },
+];
+
+export const galleryData = [
+  {
+    id: 1,
+    imageUrl: require("./assets/img1.png"),
+  },
+  {
+    id: 2,
+    imageUrl: require("./assets/img1.png"),
+  },
+  {
+    id: 3,
+    imageUrl: require("./assets/img1.png"),
+  },
+  {
+    id: 4,
+    imageUrl: require("./assets/img1.png"),
+  },
+];
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <HorizontalCarousal data={galleryData} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/components/HorizontalCarousal.js
@@ -0,0 +1,169 @@
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+
+const HorizontalCarousal = ({ data }) => {
+  const flatListRef = React.useRef(null);
+  const [slide, setSlide] = React.useState(1);
+  const [prevDisabled, setPrevDisabled] = React.useState(false);
+  const [nextDisabled, setNextDisabled] = React.useState(false);
+
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const onPrevious = () => {
+    console.log(slide, "lsakdjflkajsdlfkjalsdkfjlaksdjflkj");
+    if (activeIndex === 0) return;
+    if (flatListRef.current) {
+      flatListRef.current.scrollToIndex({
+        index: activeIndex - 1,
+      });
+    }
+  };
+  const onNext = () => {
+    console.log(slide, "lsakdjflkajsdlfkjalsdkfjlaksdjflkj");
+
+    if (activeIndex === data.length - 1) return;
+    if (flatListRef.current) {
+      flatListRef.current.scrollToIndex({
+        index: activeIndex + 1,
+      });
+    }
+  };
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center" }}>
+      <FlatList
+        ref={flatListRef}
+        // snapToInterval={SCREEN_WIDTH}
+        // snapToAlignment="center"
+        // decelerationRate={"fast"}
+        pagingEnabled={true}
+        horizontal
+        onMomentumScrollEnd={(event) => {
+          const pageIndex = Math.floor(
+            event.nativeEvent.contentOffset.x / SCREEN_WIDTH
+          );
+          setActiveIndex(pageIndex);
+        }}
+        //can i set 0 -16 scrollEventThrottle={0}
+        showsHorizontalScrollIndicator={false}
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => {
+          // if (!item.image || !item.title) return <View style={{ width: 0 }} />;
+          return (
+            <View
+              style={{
+                width: SCREEN_WIDTH,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {/* <Image source={{ uri: item.image }} style={styles.image} /> */}
+              <Image source={item?.imageUrl} style={styles.image} />
+              <Text style={styles.imageText}>{item.title}</Text>
+            </View>
+          );
+        }}
+      />
+      <View style={styles.footer}>
+        <Text
+          onPress={onPrevious}
+          disabled={prevDisabled}
+          style={styles.btnText}
+        >
+          ◀️
+        </Text>
+
+        {data.map((_, index) => (
+          <View
+            key={index}
+            style={{
+              padding: 5,
+              width: SCREEN_WIDTH - 350,
+              borderRadius: 5,
+              height: "3%",
+              backgroundColor: index === activeIndex ? "black" : "transparent",
+              borderWidth: "0.4",
+              borderColor: "black",
+            }}
+            onPress={() => handlePageChange(index)}
+          />
+        ))}
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          {/* <Ionicons
+              style={styles.galleryicon}
+              color={Colors.primaryColor}
+              name={"image-outline"}
+              size={hp("3.5")}
+            /> */}
+          <Text
+            style={{
+              textAlign: "right",
+              color: "#05253A",
+              fontSize: "10%",
+            }}
+          >
+            {`${activeIndex + 1}/${data.length}`}
+          </Text>
+        </View>
+        <Text onPress={onNext} disabled={nextDisabled} style={styles.btnText}>
+          ▶️
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default HorizontalCarousal;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "95%",
+    height: SCREEN_WIDTH - 10,
+    resizeMode: "contain",
+    borderRadius: 10,
+  },
+  imageText: {
+    position: "absolute",
+    top: 15,
+    right: 15,
+    color: "white",
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
+    alignItems: "center",
+  },
+  btnText: {
+    fontSize: 30,
+  },
+  paginationStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: "-8%",
+    backgroundColor: "rgba(255,255,255,0.35)",
+    height: "8%",
+    borderRadius: 10,
+    paddingHorizontal: "9%",
+  },
+});
